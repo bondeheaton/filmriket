507df3916bfeae45cc3a8a30732cf06bab93a074
Viktor <shorts@live.se>
2015-09-09 11:45:35 +0000

    mapachievements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -78,6 +78,39 @@ module ApplicationHelper
     return @achievement_score.inject(:+)
   end
   
+  def club_achievement_icon(club)
+    @achievement_score = [0]
+    @achievement_icon = "locked.png"
+    @users = club.users
+    if club.events
+      @achievement_score.push(club.events.count)
+    end
+    if club.users
+      @achievement_score.push(club.users.count)
+    end
+    @users.each do |user|
+      if user.reviews
+        @achievement_score.push(user.reviews.where.not(videolink: "inactive").count)
+      end
+      if user.club_movies
+        @achievement_score.push(user.club_movies.where.not(videolink: "inactive").count)
+      end
+    end
+    if @achievement_score.inject(:+) > 0
+      @achievement_icon = "bronze_medal.png"
+    end
+    if @achievement_score.inject(:+) > 10
+      @achievement_icon = "silver_medal.png"
+    end
+    if @achievement_score.inject(:+) > 20
+      @achievement_icon = "gold_medal.png"
+    end
+    if @achievement_score.inject(:+) > 30
+      @achievement_icon = "rainbow_medal.png"
+    end
+    return @achievement_icon
+  end
+  
   def current_user_helper
     return current_user
   end
