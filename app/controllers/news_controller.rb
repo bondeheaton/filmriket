8c876c944905d21cd157a6d831a95addcbaf3c64
Mikael Lisselgard <mikael.lisselgard@hotmail.se>
2015-11-07 08:46:51 +0000

    Add correct categories to news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -5,7 +5,7 @@ class NewsController < ApplicationController
   respond_to :html
 
   def index
-    @news = News.paginate(:page => params[:page], :per_page => 3)
+    @news = News.paginate(:page => params[:page], :per_page => 3).order(id: :desc)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @news }
