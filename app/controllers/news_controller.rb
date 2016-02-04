554992be8d6cd979dbda1d98281edda1b1dd8bfb
sholle <shorts@live.se>
2016-02-04 14:38:49 +0100

    Change news layout to 5 and set category after content

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,7 +4,7 @@ class NewsController < ApplicationController
   respond_to :html
 
   def index
-    @news = News.paginate(:page => params[:page], :per_page => 3).order(id: :desc)
+    @news = News.paginate(:page => params[:page], :per_page => 5).order(id: :desc)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @news }
