336579c284ee7dd9f30392d5b5da1c5d00b4696a
Mikael Lisselgard <mikael.lisselgard@hotmail.se>
2016-02-05 09:56:44 +0000

    Fix incorrect redirect edit filmschool

--- app/controllers/filmschools_controller.rb
+++ app/controllers/filmschools_controller.rb
@@ -35,7 +35,7 @@ class FilmschoolsController < ApplicationController
 
   def update
     @filmschool.update(filmschool_params)
-    respond_with(@filmschool)
+    redirect_to filmschools_path
   end
 
   def destroy
