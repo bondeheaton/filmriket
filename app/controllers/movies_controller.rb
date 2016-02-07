7ee70841ce7beb386aadd26d6882373c07731902
sholle <shorts@live.se>
2016-02-07 10:52:10 +0100

    Refactor movies_controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,24 +1,21 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
   before_action :check_admin!, only: [:edit, :update, :destroy]
+  before_action :set_ratings, only: [:index, :show]
   respond_to :html, :js
   before_filter :check_admin!, except: [:index, :show]
 
   def index
     @movies = Movie.all
     @rating = Rating.new
-    @ratings = Rating.where("movie_id = ?", params[:id])
     respond_with(@movies)
   end
 
   def show
     @rating = Rating.new
-    @ratings = Rating.where("movie_id = ?", params[:id])
-    @movie = Movie.find(params[:id])
     @booking = Booking.new
-    @bookings = @movie.bookings
     @movie_comment = MovieComment.new
-    #@movie_comments = MovieComment.where("movie_id = ?", params[:id]).reverse
+    @bookings = @movie.bookings
     @movie_comments = @movie.movie_comments.order('id DESC')
     respond_to do |format|
       format.js
@@ -52,6 +49,10 @@ class MoviesController < ApplicationController
   private
     def set_movie
       @movie = Movie.find(params[:id])
+    end
+    
+    def set_ratings
+      @ratings = Rating.where("movie_id = ?", params[:id])
     end
 
     def movie_params
