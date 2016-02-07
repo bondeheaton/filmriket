75d3a9f1f5ba399c0b8c20fa8b19c069a943457f
sholle <shorts@live.se>
2016-02-07 11:57:09 +0100

    Add comment in club_movie model

--- app/models/club_movie.rb
+++ app/models/club_movie.rb
@@ -1,9 +1,11 @@
 class ClubMovie < ActiveRecord::Base
   belongs_to :user
-
+  
+  # setting club_movie to inactive before upload is attached
   def access(user)
     self.videolink = 'inactive'
     self.active = 0
     self.user_id = user.id
   end
+  
 end
