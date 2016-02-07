928bdade5d78d85020346eb3ddade5ac5ab1227c
sholle <shorts@live.se>
2016-02-07 12:02:22 +0100

    Add comment to review-model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,12 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :movie
-
+  
+  # setting review to inactive before upload is attached
   def access(user)
     self.videolink = 'inactive'
     self.active = 0
     self.user_id = user.id
   end
+  
 end
