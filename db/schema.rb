219ecd16b7582650d4f0ee9ab688c9d9bdc97f2b
sholle <shorts@live.se>
2016-02-11 08:40:26 +0100

    Add pressdate column to presses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211065840) do
+ActiveRecord::Schema.define(version: 20160211073947) do
 
   create_table "admin_images", force: true do |t|
     t.string   "img"
@@ -136,6 +136,7 @@ ActiveRecord::Schema.define(version: 20160211065840) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "text_file"
+    t.datetime "press_date"
   end
 
   create_table "ratings", force: true do |t|
