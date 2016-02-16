eaabbba835139455977f5dca81d51d423838aaf7
Mikael Lisselgard <mikael.lisselgard@hotmail.se>
2016-02-16 09:01:01 +0000

    Refactor helper method for club achievement score to get correct score

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,10 +36,10 @@ module ApplicationHelper
     achievement_score = [0]
     achievement_score.push(club.events.count)
     achievement_score.push(club.users.count)
-    users.each do |user|
-      achievement_score.push(user.reviews.where.not(active: 0).count)
-      achievement_score.push(user.club_movies.where.not(active: 0).count)
-    end
+    achievement_score.push(club.reviews.where.not(active: 0).count)
+    achievement_score.push(club.club_movies.where.not(active: 0).count)
+    achievement_score.push(club.points)
+
     achievement_score.inject(:+)
   end
   
