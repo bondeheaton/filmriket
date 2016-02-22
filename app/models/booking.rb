8616c6054611a1e745071dd96efc36aed30859a3
sholle <shorts@live.se>
2016-02-22 09:09:19 +0100

    Refactor save_booking in booking-model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,8 +3,7 @@ class Booking < ActiveRecord::Base
   belongs_to :movie
   
   def save_booking(user)
-    club = Club.find(user.club)
-    awaiting_bookings_count = club.users.joins(:bookings).where("bookings.status = 0").count
+    awaiting_bookings_count = user.club.users.joins(:bookings).where("bookings.status = 0").count
     if awaiting_bookings_count < 3
       self.user_id = user.id
       self.status = 0
